dd0a52bcee6ec8e4f7a023632976c11c3e230ce2
YASH PASSI <98830689+kingpassi@users.noreply.github.com>
2022-07-19 18:29:36 +0530

    Add files via upload

---
+++ 62. Unique Paths(Array)(dp).cpp
@@ -0,0 +1,50 @@
+/******************************************************************************
+62. Unique Paths
+
+There is a robot on an m x n grid. The robot is initially located at the 
+top-left corner (i.e., grid[0][0]). The robot tries to move to the 
+bottom-right corner (i.e., grid[m - 1][n - 1]). The robot can only move 
+either down or right at any point in time.
+
+Given the two integers m and n, return the number of possible unique paths 
+that the robot can take to reach the bottom-right corner.
+
+The test cases are generated so that the answer will be less than or equal
+to 2 * 109.
+
+ 
+
+Example 1:
+
+
+Input: m = 3, n = 7
+Output: 28
+Example 2:
+
+Input: m = 3, n = 2
+Output: 3
+Explanation: From the top-left corner, there are a total of 3 ways to
+reach the bottom-right corner:
+1. Right -> Down -> Down
+2. Down -> Down -> Right
+3. Down -> Right -> Down
+*******************************************************************************/
+class Solution {
+    
+    int helper( int m, int n, vector<vector<int>>&dp){
+       if(m==0 && n==0) return 1;
+        if(m<0 || n<0) return 0;
+        
+        if(dp[m][n]!=-1) return dp[m][n];
+        
+        else return dp[m][n]=helper(m-1,n,dp)+helper(m,n-1,dp);
+        
+    }
+    
+    
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>>dp(m+1,vector(n+1,-1));
+        return helper(m-1,n-1,dp);
+    }
+};
